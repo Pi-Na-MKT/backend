c63e77ae8e99446d759c1e4dd1a65939f4399579
EnzodeFreitas <enzo.freitas@sptech.school>
2026-05-03 19:32:50 -0300

    fix: ajustando lógica delete e put

--- src/main/java/com/pina/mkt_api/security/SecurityConfig.java
+++ src/main/java/com/pina/mkt_api/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
@@ -35,7 +36,8 @@ public class SecurityConfig {
                                 "/api/users/register",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/h2-console/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
